d1f95b372dc0c7f9acba9239b8cb9442febd0210
simpan824 <simon.dagernas@elev.ga.ntig.se>
2023-01-30 09:23:09 +0100

    stuffs

--- palindrom/Program.cs
+++ palindrom/Program.cs
@@ -1,14 +1,11 @@
 ﻿/*
 * Input layer: ta in användarens input och spara den i en array för en plats per bokstav. 
 * 
-* Verification layer: tror inte jag behöver göra något här om jag inte vidareutvecklar det mer
-* just nu tar den med alla tecken i unicode men om jag blir klar i tid så kan den bli restricted till bara bokstäver
+* Verification layer: omvandla alla bokstäver till lowercase
 *
 * Data layer: kolla varje bokstav mot den sista bokstaven i ordet och sedan gå till nästa och kolla näst sista. 
 * om ordet är anna så kollar position 0 och position 3 och sedan ta position++; och anna.length--;
 * lägg detta i en for loop som loopar n antal gånger baserat på hur många bokstäver ordet innehåller
-* 
-* (jag ska omvandla den här texten till något normala människor också kan förstå och inte bara jag)
 * 
 * Output layer: skriv ut om användarens ord är en palindrom eller inte. 
 */
@@ -19,31 +16,86 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace palindrom {
-    class Program {
-        static void Main(string[] args) {
+namespace palindrom 
+{
+    class Program 
+    {
+        static void Main(string[] args) 
+        {
+            Userinput();
+        }
+        static void Userinput()
+        {
             string userinput;
-            int startnum = 0;
-            bool palindromstatus;
+
             Console.WriteLine("skriv ett ord det kanske är en palindrom vem vet :oooo");
-
             userinput = Console.ReadLine();
-            char[] Palindrome = userinput.ToCharArray();
-            int pLength = Palindrome.Length;
+            userinput = userinput.ToLower(); //gör userinput till lowercase för uppercase of lowercase bokstäver är  två olika tecken
 
-            for (int i = 0; i < pLength; i++) {
-                if (Palindrome[startnum] == Palindrome[pLength-1]) {
-                    Console.WriteLine("true");
-                    pLength--;
-                    startnum++;
+            Palindromcheck(userinput);
+        }
+
+        static void Palindromcheck(string userinput)
+        {
+            int startnum = 0;
+            int uinputlength;
+            bool palindromstatus = false;
+
+            string[] userinputs = userinput.Split(' ');
+            uinputlength = userinputs.Length;
+
+            for (int i = 0; i < uinputlength; i++)
+            {
+                userinput = userinputs[0].ToString();
+                char[] palindrome = userinput.ToCharArray(); //gör userinput till en array
+                int pLength = palindrome.Length; //bestämmer längden på arrayen
+
+                for (int j = 0; j < pLength; j++) //loopar n antal gånger utifrån hur många tecken som finns i arrayen
+                {
+                    if (palindrome[startnum] == palindrome[pLength - 1]) //kollar om första tecknet i userinput är samma som det sista och sedan kollar samma sak om nästa och näst sista
+                    {
+                        pLength--;
+                        startnum++;
+                        palindromstatus = true;
+                    }
+                    else
+                    {
+                        palindromstatus = false;
+                        i = pLength;
+                    }
+                } //slutar loopa när den jämfört de ett eller två mittersta tecknen
+
+                if (palindromstatus == false)
+                {
+                    Console.WriteLine(userinput + " är inte en palindrom jag gråtar nu");
                 }
-                else {
-                    Console.WriteLine("ditt ord är inte en palindrom jag gråtar nu");
-                    palindromstatus = false;
+                else if (palindromstatus == true)
+                {
+                    Console.WriteLine(userinput + " has been successfully palindromed");
                 }
+                else
+                {
+                    Console.WriteLine("idek how you got here tbh");
+                } //ger svar till användaren om ordet är en palindrom eller inte 
             }
 
-            Console.ReadKey();
+            Continue();
+        }
+
+        static void Continue() //kollar om användaren vill fortsätta eller inte
+        {
+            Console.WriteLine("continue? y/n");
+            string userinput = Console.ReadLine();
+
+            if(userinput == "y")
+            {
+                Userinput();
+            }
+            else
+            {
+                Console.WriteLine("bye"); 
+                Console.ReadKey();
+            }
         }
     }
 }
